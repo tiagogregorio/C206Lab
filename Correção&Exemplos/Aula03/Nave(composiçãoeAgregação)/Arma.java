aec0f056214c36af6270b4eac140140fc8c866bd
tiagogregorio <126029094+tiagogregorio@users.noreply.github.com>
2023-03-18 19:55:25 -0300

    Arma.java

---
+++ Correção&Exemplos/Aula03/Nave(composiçãoeAgregação)/Arma.java
@@ -0,0 +1,13 @@
+public class Arma {
+    //atributos da nave
+
+    String tipo;
+    int forca;
+
+    //construtor da nave
+    public Arma(String tipo, int forca) {
+        System.out.println("Arma criada!!");
+        this.tipo = tipo;
+        this.forca = forca;
+    }
+}
