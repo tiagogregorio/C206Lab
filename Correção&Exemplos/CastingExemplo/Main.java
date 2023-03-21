8144ac4b76d5fc3e6f749f82ec570a45a55d796b
tiagogregorio <126029094+tiagogregorio@users.noreply.github.com>
2023-03-21 14:13:34 -0300

    Create Main.java

---
+++ Correção&Exemplos/CastingExemplo/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double numDouble = 10.5;
+        int numInteiro = (int) numDouble;
+        System.out.println("O valor do double é: " + numDouble);
+        System.out.println("O valor do inteiro é: " + numInteiro);
+    }
+}
