11d01b31136270d46a556f1d36b27fe0a21e86b4
tiagogregorio <126029094+tiagogregorio@users.noreply.github.com>
2023-03-23 21:27:44 -0300

    Create MemoriaUSB.java

---
+++ PcMania/MemoriaUSB.java
@@ -0,0 +1,15 @@
+public class MemoriaUSB {
+
+    //#region Atributos
+    String nome;
+    int capacidade;
+
+    //#endregion
+
+    //#region Construtores
+    public MemoriaUSB(String nome, int capacidade) {
+        this.nome = nome;
+        this.capacidade = capacidade;
+    }
+    //#endregion
+}
