f13e925996afe8f397a6eac9ea6c1f389783a53f
tiagogregorio <126029094+tiagogregorio@users.noreply.github.com>
2023-03-23 21:28:14 -0300

    Create SistemaOperacional.java

---
+++ PcMania/SistemaOperacional.java
@@ -0,0 +1,14 @@
+public class SistemaOperacional {
+
+    //#region Atributos
+    String nome;
+    int tipo;
+    //#endregion
+
+    //#region Construtores
+    public SistemaOperacional(String nome, int tipo) {
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+    //#endregion
+}
