ab023a401bc1507940c61d10507b5dc69344e08c
tiagogregorio <126029094+tiagogregorio@users.noreply.github.com>
2023-03-23 21:26:59 -0300

    Create HardwareBasico.java

---
+++ PcMania/HardwareBasico.java
@@ -0,0 +1,14 @@
+public class HardwareBasico {
+
+    //#region Atributos
+    String nome;
+    float capacidade;
+    //#endregion
+
+    //#region Construtores
+    public HardwareBasico(String nome, float capacidade) {
+        this.nome = nome;
+        this.capacidade = capacidade;
+    }
+    //#endregion
+}
