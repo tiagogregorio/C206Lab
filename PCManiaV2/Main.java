5a3a9cc7fb809ce91c69c6f5bdb1b815c1594a2e
tiagogregorio <126029094+tiagogregorio@users.noreply.github.com>
2023-03-26 19:29:33 -0300

    Create Main.java

---
+++ PCManiaV2/Main.java
@@ -0,0 +1,116 @@
+import java.util.Scanner;
+
+public class Main {
+    public static <nome1> void main(String[] args) {
+
+        Cliente client = new Cliente();//Nome e CPF do Cliente
+        client.nome = "Ze do Bit Carry";
+        client.cpf = 245634823;
+
+        float valortotal = 0; //Armazenar a soma do valor de todos os PCs vendidos
+
+        //#region Estanciando Computador
+        Computador computador1 = new Computador();
+        Computador computador2 = new Computador();
+        Computador computador3 = new Computador();
+        //#endregion
+
+        //#region Menu
+        Scanner sc = new Scanner(System.in);
+        int opcao = -1;
+        while (opcao != 0) {
+            System.out.println("Escolha uma opção:");
+            System.out.println("Digite 1 para comprar computador que esta na Promoção 1 ");
+            System.out.println("Digite 2 para comprar computador que esta na Promoção 2 ");
+            System.out.println("Digite 3 para comprar computador que esta na Promoção 3 ");
+            System.out.println("Digite 0 para fechar compra");
+
+            opcao = sc.nextInt();
+            sc.nextLine();//limpar o buffer
+            switch (opcao) {
+                case 0:
+                    System.out.println("----------------- Produtos ----------------- ");
+
+                    if (client.comp1 > 0) {
+                        System.out.println("\nPC(s) da Promoçao 1 - Quantidade: " + client.comp1 + " Item(s)");
+                        computador1.mostraPCConfigs();
+                        valortotal = (client.comp1 * computador1.preco) + (client.comp2 * computador2.preco) + (client.comp3 * computador3.preco);
+                    }
+                    if (client.comp2 > 0) {
+                        System.out.println("\nPC(s) da Promoçao 2 - Quantidade: " + client.comp2 + " Item(s)");
+                        computador2.mostraPCConfigs();
+                        valortotal = (client.comp1 * computador1.preco) + (client.comp2 * computador2.preco) + (client.comp3 * computador3.preco);
+                    }
+                    if (client.comp3 > 0) {
+                        System.out.println("\nPC(s) da Promoçao 3 - Quantidade: " + client.comp3 + " Item(s)");
+                        computador3.mostraPCConfigs();
+                        valortotal = (client.comp1 * computador1.preco) + (client.comp2 * computador2.preco) + (client.comp3 * computador3.preco);
+                    }
+                    System.out.println("\n----------------- Pagador -----------------");
+                    System.out.println("Nome: " + client.nome + "       CPF: " + client.cpf);
+
+                    client.conta = valortotal;
+                    System.out.printf("Valor a pagar (Total) = R$ %.2f\n", client.calculaTotalCompra());
+                    System.out.println("------------------------------------------");
+                    System.out.println("         Obrigado!Volte Sempre!  ");
+                    break;
+
+                case 1:
+                    System.out.println("Adicionando computador da Promoção 1 no carrinho...\n");
+                    MemoriaUSB memoria1 = new MemoriaUSB(); //Criando memória nome
+                    memoria1.nome[0] = "Pen-drive"; //Atribuindo valores para Memória
+                    memoria1.capacidade[0] = 16;
+                    computador1.memoria = memoria1;//Associação
+                    client.comp1++;//Contador de computador1
+                    computador1.sisoperacional.nome = "Linux Ubuntu";
+                    computador1.sisoperacional.tipo = 32;
+                    computador1.hardwareb.nome[0] = "Pentium Core i3, Memória RAM, HD ";
+                    computador1.hardwareb.capacidade[0] = 2200f; //Processador
+                    computador1.hardwareb.capacidade[1] = 8f; //Ram
+                    computador1.hardwareb.capacidade[2] = 500f; // HD
+                    computador1.marca = "Positivo";
+                    computador1.preco = 2300;
+                    break;
+
+                case 2:
+                    MemoriaUSB memoria2 = new MemoriaUSB(); //Criando memória 2
+                    memoria2.nome[1] = "Pen-drive"; //Atribuindo nome memoria 2
+                    memoria2.capacidade[1] = 32; // Capacidade mmemoria 2
+                    computador2.memoria = memoria2; // Associando computador 2
+                    computador2.sisoperacional.nome = "Windows 8";
+                    computador2.sisoperacional.tipo = 64;
+                    System.out.println("Adicionando computador da Promoção 2 no carrinho...\n");
+                    client.comp2++;//Contador de computador2
+                    computador2.hardwareb.nome[1] = "Pentium Core i5, Memória RAM, HD ";
+                    computador2.hardwareb.capacidade[0] = 3370f; //Processador
+                    computador2.hardwareb.capacidade[1] = 16f; //Ram
+                    computador2.hardwareb.capacidade[2] = 1000f; // HD
+                    computador2.marca = "Acer";
+                    computador2.preco = 5800;
+                    break;
+
+                case 3:
+                    MemoriaUSB memoria3 = new MemoriaUSB(); //Criando memória
+                    memoria3.nome[2] = "HD externo"; // Atribuindo memoria 3
+                    memoria3.capacidade[2] = 1000; // Capacidade memoria 3
+                    computador3.memoria = memoria3; // associação
+                    computador3.sisoperacional.nome = "Windows 10";
+                    computador3.sisoperacional.tipo = 64;
+                    System.out.println("Adicionando computador da Promoção 3 no carrinho...\n");
+                    client.comp3++;//Contador de computador3
+                    computador3.hardwareb.nome[2] = "Pentium Core i7, Memória RAM, HD ";
+                    computador3.hardwareb.capacidade[0] = 4500f; //Processador
+                    computador3.hardwareb.capacidade[1] = 32f; //Ram
+                    computador3.hardwareb.capacidade[2] = 2000f; // HD
+                    computador3.marca = "Vaio";
+                    computador3.preco = 1800;
+                    break;
+
+                default:
+                    System.out.println("Opção inválida.");
+            }
+        }
+        sc.close();
+        //#endregion
+    }
+}
