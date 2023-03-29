6cf89a42cfb629b055d98b6ccc22a15db08aa52f
tiagogregorio <126029094+tiagogregorio@users.noreply.github.com>
2023-03-29 00:17:36 -0300

    Update Cliente.java

--- PCManiaV2/Cliente.java
+++ PCManiaV2/Cliente.java
@@ -1,15 +1,29 @@
 public class Cliente {
 
-    //#region Atributos
-    String nome;
-    long cpf;
-    int comp1 = 0, comp2 = 0, comp3 = 0;//Contador para verificarmos quantos computadores foram vendidos
-    float conta;
+    //#region Declaração dos atributos públicos nome, cpf do cliente e do array de computadores
+    public String nome;
+    public long cpf;
+    Computador[] computadores = new Computador[10];
+    //#endregion
+
+    //#region Construtores
+    public Cliente() {
+        this.nome = nome;
+        this.cpf = cpf;
+
+    }
     //#endregion
 
     //#region Metodos
     float calculaTotalCompra() {
-        return conta;
+        //Declaração da variável somaTotal, que irá armazenar o valor total da compra
+        float somaTotal = 0;
+
+        for (int i = 0; i < computadores.length; i++) { //Percorre o array de computadores
+            if (computadores[i] != null) { //Verifica se o computador na posição atual não é nulo
+                somaTotal += computadores[i].preco; ////Adiciona o preço do computador na somaTotal
+            }
+        }
+        return somaTotal; ////Retorna o valor total da compra
     }
-    //#endregion
-}
+}   //#endregion
