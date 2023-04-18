6c0469f2ec9d9ae750a96250e5d06a0dfc3f9ee0
tiagogregorio <126029094+tiagogregorio@users.noreply.github.com>
2023-04-18 16:45:40 -0300

    Update Habitante.java

--- SenhorDosAneis/Habitante.java
+++ SenhorDosAneis/Habitante.java
@@ -1,18 +1,23 @@
 public abstract class Habitante { //Habitantes com classe abstrata
+    // Características gerais dos habitantes da terra média
     public static int contador = 0; //Contador Estatico
     protected int id;
     protected String nome;
     protected int idade;
     protected float energia;
+    //Agregação
+    Arma arma; //Habitante pode ter uma arma ou não
 
-    public Habitante(int id, String nome, int idade, float energia) {
-        this.id = id;
+    //Construtor da classe Habitante
+    public Habitante(String nome, int idade, float energia) {
         this.nome = nome;
         this.idade = idade;
         this.energia = energia;
         contador++;//incrementa a quantidade de habitantes criados
+        this.id = contador;
     }
 
+    //Getter contador de habitantes
     public static int getContador() {
         if (contador == 0) {
             System.out.println("Não existem habitantes!");
@@ -20,11 +25,18 @@ public abstract class Habitante { //Habitantes com classe abstrata
         return contador;
     }
 
+    // Métodos gerais dos habitantes da terra média
     public void atacar() {
-
+        System.out.println("Arma utilizada: " + arma.getNomeArma());
+        if (arma.isMagica()) {//ja testa se é verdadeiro
+            energia -= 20;
+            System.out.println("A arma é mágica, energia gasta (20)");
+        } else {
+            energia -= 10;
+            System.out.println("A arma não é magica, energia gasta (10)");
+        }
+        System.out.println("Energia atual: " + energia);
     }
-
-    public abstract void curar();
 
     public void mostraInfo() {
         System.out.println("Identificação: " + id);
