e3c268360756af63e9a00230016db48f8a548ad6
tiagogregorio <126029094+tiagogregorio@users.noreply.github.com>
2023-04-18 16:44:59 -0300

    Update Elfo.java

--- SenhorDosAneis/Elfo.java
+++ SenhorDosAneis/Elfo.java
@@ -1,31 +1,28 @@
 public class Elfo extends Habitante implements Cura {
+    // Atributos extras de um elfo
     private String tribo;
 
-    public Elfo(int id, String nome, int idade, float energia) {
-        super(id, nome, idade, energia);
+    public Elfo(String nome, int idade, float energia) {
+        super(nome, idade, energia);
     }
 
     public void setTribo(String tribo) {
         this.tribo = tribo;
     }
 
+    // Metódos do elfo
     public void viajar() {
         System.out.println("O Elfo " + this.nome + " esta viajando...");
-
     }
 
+    // Métodos reescritos da classe mãe (Habitante)
     @Override
     public void atacar() {
         System.out.println("\nElfo esta atacando...");
-
-        if (Arma.getmagica() == false) {
-            energia -= 20;
-        } else {
-            energia -= 10;
-        }
-        System.out.println("Energia: " + energia);
+        super.atacar(); ////Usando o super conseguimos reaproveitar o código
     }
 
+    @Override // Método implementado a partir da interface de cura
     public void curar() {
         System.out.println("Elfo foi curado em 15%, energia atual: " + (energia * 0.15 + energia));
     }
@@ -33,7 +30,7 @@ public class Elfo extends Habitante implements Cura {
     @Override
     public void mostraInfo() {
         System.out.println("\n--- Infos do Elfo ---");
-        super.mostraInfo();
-        System.out.println("Tribo Elfo: " + tribo);
+        super.mostraInfo();  //Usando o super conseguimos reaproveitar o mostraInfo de habitantes
+        System.out.println("Tribo Elfo: " + tribo); // adicionamos as particularidades do elfo
     }
 }
