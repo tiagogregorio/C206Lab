fd84d3be537fb7c63fe3c78ce3d263e056a12c3d
tiagogregorio <126029094+tiagogregorio@users.noreply.github.com>
2023-04-18 16:46:39 -0300

    Update Main.java

--- SenhorDosAneis/Main.java
+++ SenhorDosAneis/Main.java
@@ -1,28 +1,31 @@
 public class Main {
     public static void main(String[] args) {
 
-        Anao anao1 = new Anao(Habitante.contador, "Feliz", 20, 1000);
+        //Criando os herois e suas armas
+        Anao anao1 = new Anao("Feliz", 20, 1000);
         anao1.setAltura(120);
         anao1.setReino("Tormenta");
         Arma anaoarma = new Arma("Machado", false);
+        anao1.arma = anaoarma;
 
-        Elfo elfo1 = new Elfo(Habitante.contador, "Legolas", 120, 5000);
+        Elfo elfo1 = new Elfo("Legolas", 120, 5000);
         elfo1.setTribo("Tolkien");
         Arma elfoarma = new Arma("Arco", false);
+        elfo1.arma = elfoarma;
 
-        Mago mago1 = new Mago(Habitante.contador, "Karthus", 1000, 10000);
+        Mago mago1 = new Mago("Karthus", 1000, 10000);
         mago1.setCor("Black");
         Arma magoarma = new Arma("Cajado", true);
+        mago1.arma = magoarma;
 
-        TerraMedia terra = new TerraMedia(); //Instanciando a Terra
-        //Adicionando os objetos
+        TerraMedia terra = new TerraMedia(); //Instanciando "criando" a TerraMedia
+        //Adicionando os objetos, os heróis a terra media
         terra.addHabitante(anao1);
         terra.addHabitante(elfo1);
         terra.addHabitante(mago1);
 
-        terra.listarHabitantes();
+        terra.listarHabitantes(); // Mostrando as informações de habitantes
 
         System.out.println("Quantidade Total de Habitantes: " + Habitante.getContador());
-
     }
 }
