f32ffcb241ff0874a03bb7d97e35dc70b6b6da5d
tiagogregorio <126029094+tiagogregorio@users.noreply.github.com>
2023-05-07 23:02:26 -0300

    Create CupomDesconto.java

---
+++ Biscoito/CupomDesconto.java
@@ -0,0 +1,11 @@
+public class CupomDesconto {
+    private double valorDesconto;
+
+    public CupomDesconto(double valorDesconto) {
+        this.valorDesconto = valorDesconto;
+    }
+
+    public  double getValorDesconto() {
+        return valorDesconto;
+    }
+}
