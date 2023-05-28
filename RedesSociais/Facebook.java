9b0a81ad8f48f1d9ccbbdae505a56b51cd140c3e
tiagogregorio <126029094+tiagogregorio@users.noreply.github.com>
2023-05-28 14:51:05 -0300

    Create Facebook.java

---
+++ RedesSociais/Facebook.java
@@ -0,0 +1,58 @@
+public class Facebook extends RedeSocial implements VideoConferencia, Compartilhamento {
+    //#region reescritas de Metodos
+    @Override
+    public void postarFoto() {
+        try {
+            System.out.println("Postou uma foto no Facebook");
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro ao postar a foto no Facebook: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void postarVideo() {
+        try {
+            System.out.println("Postou um vídeo no Facebook");
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro ao postar o vídeo no Facebook: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void postarComentario() {
+        try {
+            System.out.println("Postou um comentário no Facebook");
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro ao postar o comentário no Facebook: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void curtirPublicacao() {
+        try {
+            super.curtirPublicacao();
+            System.out.println("no Facebook");
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro ao curtir a publicação no Facebook: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void compartilhar() {
+        try {
+            System.out.println("Compartilhou uma publicação no Facebook");
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro ao compartilhar a publicação no Facebook: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void fazStreaming() {
+        try {
+            System.out.println("Realizou uma videoconferência no Facebook");
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro ao fazer a videoconferência no Facebook: " + e.getMessage());
+        }
+    }
+    //#endregion
+}
