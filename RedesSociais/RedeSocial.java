bb0abac9809c17587ecf82097666464cae3c3a48
tiagogregorio <126029094+tiagogregorio@users.noreply.github.com>
2023-05-28 14:54:13 -0300

    Create RedeSocial.java

---
+++ RedesSociais/RedeSocial.java
@@ -0,0 +1,21 @@
+public abstract class RedeSocial { //A Classe RedeSocial é abstrata
+    //#region Atributos
+    protected String senha;
+    protected int numAmigos;
+    //#endregion
+
+    //#region Metodos
+    //#region Metodos publicos e abstratos (estao em italico na UML)
+    abstract public void postarFoto(); // Método abstrato para postar foto
+    abstract public void postarVideo(); // Método abstrato para postar vídeo
+    abstract public void postarComentario(); // Método abstrato para postar comentário
+    //#endregion
+
+    //#region Metodo publico e NAO abstrato
+    public void curtirPublicacao() {
+        System.out.print("Curtiu uma publicacao "); // Método não abstrato para curtir uma publicação
+    }
+}
+//#endregion
+
+//#endregion
