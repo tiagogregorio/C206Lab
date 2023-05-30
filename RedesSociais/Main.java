6028d7097b0c3b0a20908e76b8bae489de882a21
tiagogregorio <126029094+tiagogregorio@users.noreply.github.com>
2023-05-30 16:54:26 -0300

    Update Main.java

--- RedesSociais/Main.java
+++ RedesSociais/Main.java
@@ -1,15 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         // Criação do objeto "usuario1" da classe Usuario com nome e email
-        Usuario usuario1 = new Usuario();
-        usuario1.setNome("Javarlindo");
-        usuario1.setEmail("javarlindo@inatel.br");
+
 
         // Criação dos objetos das redes sociais com suas respectivas senhas e número de amigos
-        Facebook facebook1 = new Facebook();
-        Twitter twitter1 = new Twitter();
-        // Instagram instagram1 = new Instagram();
-        //GooglePlus googlePlus1 = new GooglePlus();
+        Facebook facebook1 = new Facebook("face123", 238);
+        Twitter twitter1 = new Twitter("twitter123", 321);
+        //  GooglePlus googlePlus1 = new GooglePlus("google123", 100);
+        // Instagram instagram1 = new Instagram("insta123", 200);
+
+        RedeSocial[] redes = new RedeSocial[4];
+        redes[0] = twitter1;
+        redes[1] = facebook1;
+        //  redes[2]=googlePlus1;
+        //  redes[3]=instagram1;
+
+        Usuario usuario1 = new Usuario(redes);
+        usuario1.setNome("nomedousuario");
+        usuario1.setEmail("nomedousuario@email.com.br");
 
         // Imprime informações do usuário
         System.out.println("==================================");
@@ -17,16 +25,9 @@ public class Main {
         System.out.println("Email: " + usuario1.getEmail());
         System.out.println("==================================");
 
-        // Adiciona as redes sociais ao usuário
-        usuario1.Usuario(facebook1);
-        usuario1.Usuario(twitter1);
-        //  usuario1.Usuario(instagram1);
-        //  usuario1.Usuario(googlePlus1);
-
         // Loop for para percorrer as redes sociais do usuário
         for (int i = 0; i < usuario1.redeSocial.length; i++) {
             if (usuario1.redeSocial[i] != null) { //Se diferente de null
-
                 //Caso ocorra uma exceção durante a execução dos métodos ou criação dos objetos, o bloco catch correspondente será acionado
                 // exibindo uma mensagem de erro e permiti tomar ações específicas para lidar com o problema.
                 if (usuario1.redeSocial[i] instanceof Facebook) {
@@ -37,7 +38,6 @@ public class Main {
                         face.postarFoto();
                         face.postarVideo();
                         face.fazStreaming();
-                        System.out.println("----------------------------------");
                     } catch (Exception e) {
                         System.out.println("Ocorreu um erro ao executar os métodos do Facebook: " + e.getMessage());
                     }
@@ -49,7 +49,6 @@ public class Main {
                         tw.curtirPublicacao();
                         tw.compartilhar();
                         tw.postarComentario();
-                        System.out.println("----------------------------------");
                     } catch (Exception e) {
                         System.out.println("Ocorreu um erro ao executar os métodos do Twitter: " + e.getMessage());
                     }
