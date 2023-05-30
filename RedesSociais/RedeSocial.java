ec5b28b99f9563428ca1fb31dc8565642a7bad42
tiagogregorio <126029094+tiagogregorio@users.noreply.github.com>
2023-05-30 16:54:54 -0300

    Update RedeSocial.java

--- RedesSociais/RedeSocial.java
+++ RedesSociais/RedeSocial.java
@@ -4,10 +4,18 @@ public abstract class RedeSocial { //A Classe RedeSocial é abstrata
     protected int numAmigos;
     //#endregion
 
-    //#region Metodos
+    //#region construtores
+    public RedeSocial(String senha, int numAmigos) {
+        this.senha = senha;
+        this.numAmigos = numAmigos;
+    }
+    //#endregion
+
     //#region Metodos publicos e abstratos (estao em italico na UML)
     abstract public void postarFoto(); // Método abstrato para postar foto
+
     abstract public void postarVideo(); // Método abstrato para postar vídeo
+
     abstract public void postarComentario(); // Método abstrato para postar comentário
     //#endregion
 
@@ -15,7 +23,5 @@ public abstract class RedeSocial { //A Classe RedeSocial é abstrata
     public void curtirPublicacao() {
         System.out.print("Curtiu uma publicacao "); // Método não abstrato para curtir uma publicação
     }
-}
-//#endregion
+}    //#endregion
 
-//#endregion
