6bc086da40d258d6d300841f040c97ff642ab17b
tiagogregorio <126029094+tiagogregorio@users.noreply.github.com>
2023-06-20 22:58:40 -0300

    Create Cliente.java

---
+++ ProjetoFinal/Cliente.java
@@ -0,0 +1,12 @@
+package Model;
+
+public class Cliente {
+    public int Contador = 0;
+    protected String Nome;
+    protected int id;
+    protected String cpf;
+    protected String email;
+
+    public Cliente() {
+    }
+}
