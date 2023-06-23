126bdec8b6900f2a9c67d8d79c2a6b7d9d928e86
tiagogregorio <126029094+tiagogregorio@users.noreply.github.com>
2023-06-23 11:18:46 -0300

    Create Liquidez.java

---
+++ ProjetoFinal/Liquidez.java
@@ -0,0 +1,10 @@
+public interface Liquidez {
+    // Interface que define o contrato para classes que possuem o método "liquidez"
+
+    void liquidez();
+    // Método que representa a liquidez de um ativo financeiro
+
+    // Observação: Interfaces não possuem implementações de métodos, apenas declarações.
+    // As classes que implementarem essa interface deverão fornecer uma implementação
+    // para o método "liquidez" de acordo com suas necessidades específicas.
+}
