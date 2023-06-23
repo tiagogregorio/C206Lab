813967bdc6565ba6c2deb179342e5f65240dc729
tiagogregorio <126029094+tiagogregorio@users.noreply.github.com>
2023-06-23 11:19:37 -0300

    Update Poupanca.java

--- ProjetoFinal/Poupanca.java
+++ ProjetoFinal/Poupanca.java
@@ -1,8 +1,14 @@
-package Model;
+class Poupanca extends RendaFixa {
+    // Classe que representa o tipo de investimento em Poupança
 
-public class Poupanca {
-    String protecao;
+    public Poupanca(String nome, int valorInvest) {
+        super(nome, valorInvest);
+        // Chama o construtor da classe pai (RendaFixa) passando o nome e valor do investimento
+    }
 
-    public Poupanca() {
+    @Override
+    public String getProdutos() {
+        return "Poupança";
+        // Retorna o nome do produto de investimento como "Poupança"
     }
 }
